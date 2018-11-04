fd8b28211aa8d06fb1fa0f630653fec7c427cf0f
anubh-v <e0176401@u.nus.edu>
2018-11-04 10:23:57 +0800

    Add missing category test

--- src/test/java/seedu/address/storage/entry/XmlSerializableEntryBookTest.java
+++ src/test/java/seedu/address/storage/entry/XmlSerializableEntryBookTest.java
@@ -12,6 +12,7 @@ import org.junit.rules.ExpectedException;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.XmlUtil;
 import seedu.address.model.EntryBook;
+import seedu.address.model.category.Category;
 import seedu.address.model.entry.EntryInfo;
 import seedu.address.testutil.TypicalEntrys;
 
@@ -21,6 +22,7 @@ public class XmlSerializableEntryBookTest {
     private static final Path TYPICAL_ENTRIES_FILE = TEST_DATA_FOLDER.resolve("typicalEntryBook.xml");
     private static final Path INVALID_ENTRYTITLE_FILE = TEST_DATA_FOLDER.resolve("invalidEntryTitle.xml");
     private static final Path MISSING_BULLETS_FILE = TEST_DATA_FOLDER.resolve("missingBullets.xml");
+    private static final Path MISSING_CATEGORY_FILE = TEST_DATA_FOLDER.resolve("missingCategory.xml");
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -47,12 +49,25 @@ public class XmlSerializableEntryBookTest {
     }
 
     @Test
-    public void toModelType_invalidDescription_failure() throws Exception {
+    public void toModelType_missingBullets_failure() throws Exception {
         XmlSerializableEntryBook dataFromFile = XmlUtil.getDataFromFile(MISSING_BULLETS_FILE,
                 XmlSerializableEntryBook.class);
 
         thrown.expect(IllegalValueException.class);
         thrown.expectMessage(XmlAdaptedEntryDescription.MESSAGE_MISSING_BULLETS);
+
+        dataFromFile.toModelType();
+
+    }
+
+    @Test
+    public void toModelType_missingCategory_failure() throws Exception {
+        XmlSerializableEntryBook dataFromFile = XmlUtil.getDataFromFile(MISSING_CATEGORY_FILE,
+                XmlSerializableEntryBook.class);
+
+        thrown.expect(IllegalValueException.class);
+        thrown.expectMessage(String.format(XmlAdaptedResumeEntry.MISSING_FIELD_MESSAGE_FORMAT,
+                                                   Category.class.getSimpleName()));
 
         dataFromFile.toModelType();
 
