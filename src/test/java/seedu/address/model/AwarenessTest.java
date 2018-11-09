c81c60fc58614d4914ba0cf799d14bd9024a41ef
anubh-v <e0176401@u.nus.edu>
2018-11-09 22:42:04 +0800

    Reorder import

--- src/test/java/seedu/address/model/AwarenessTest.java
+++ src/test/java/seedu/address/model/AwarenessTest.java
@@ -1,7 +1,7 @@
 package seedu.address.model;
 
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.util.TreeMap;
 
