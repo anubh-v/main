6be1058ff660826cb5f17f423be37f19ff9b5296
anubh-v <e0176401@u.nus.edu>
2018-11-10 02:23:31 +0800

    Add tests for XmlSerializableAwareness

---
+++ src/test/java/seedu/address/storage/awareness/XmlSerializableAwarenessTest.java
@@ -0,0 +1,51 @@
+package seedu.address.storage.awareness;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.util.XmlUtil;
+import seedu.address.model.AddressBook;
+import seedu.address.storage.XmlSerializableAddressBook;
+import seedu.address.storage.XmlSerializableAwareness;
+import seedu.address.testutil.TypicalPersons;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
+public class XmlSerializableAwarenessTest {
+
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "XmlSerializableAwarenessTest");
+    private static final Path MISSING_CONTEXTENTRY_FILE = TEST_DATA_FOLDER.resolve("missingContextEntry.xml");
+    private static final Path DUPLICATE_EVENTNAMES_FILE = TEST_DATA_FOLDER.resolve("duplicateEventNames.xml");
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void toModelType_typicalAwarenessFile_success() throws Exception {
+      // wip to add positive test case
+    }
+
+    @Test
+    public void toModelType_missingContextEntryFile_throwsIllegalValueException() throws Exception {
+        XmlSerializableAwareness dataFromFile = XmlUtil.getDataFromFile(MISSING_CONTEXTENTRY_FILE,
+                XmlSerializableAwareness.class);
+
+        thrown.expect(IllegalValueException.class);
+        dataFromFile.toModelType();
+    }
+
+    @Test
+    public void toModelType_duplicateEventNames_throwsIllegalValueException() throws Exception {
+        XmlSerializableAwareness dataFromFile = XmlUtil.getDataFromFile(DUPLICATE_EVENTNAMES_FILE,
+                XmlSerializableAwareness.class);
+
+        thrown.expect(IllegalValueException.class);
+        dataFromFile.toModelType();
+    }
+
+
+}
