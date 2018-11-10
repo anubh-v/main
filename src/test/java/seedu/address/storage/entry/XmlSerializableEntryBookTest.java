44ff5f1fabd7c3845942da50c1e3625971527f0e
anubh-v <e0176401@u.nus.edu>
2018-11-10 12:51:50 +0800

    Add header comment

--- src/test/java/seedu/address/storage/entry/XmlSerializableEntryBookTest.java
+++ src/test/java/seedu/address/storage/entry/XmlSerializableEntryBookTest.java
@@ -16,6 +16,8 @@ import seedu.address.model.category.Category;
 import seedu.address.model.entry.EntryInfo;
 import seedu.address.testutil.TypicalEntrys;
 
+/** Integration tests to test overall Entrybook loading. Each XML component is tested individually for more specific
+ * test cases.  */
 public class XmlSerializableEntryBookTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "XmlSerializableEntryBookTest");
