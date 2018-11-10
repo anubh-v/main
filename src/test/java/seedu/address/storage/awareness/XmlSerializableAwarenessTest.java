334fe031582578b5cbab303b641b245df788303d
anubh-v <e0176401@u.nus.edu>
2018-11-10 09:27:30 +0800

    Fix indent level

--- src/test/java/seedu/address/storage/awareness/XmlSerializableAwarenessTest.java
+++ src/test/java/seedu/address/storage/awareness/XmlSerializableAwarenessTest.java
@@ -14,9 +14,11 @@ import seedu.address.storage.XmlSerializableAwareness;
 public class XmlSerializableAwarenessTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "XmlSerializableAwarenessTest");
+
     private static final Path MISSING_CONTEXTENTRY_FILE = TEST_DATA_FOLDER.resolve("missingContextEntry.xml");
     private static final Path DUPLICATE_EVENTNAMES_FILE = TEST_DATA_FOLDER.resolve("duplicateEventNames.xml");
     private static final Path EMPTY_EVENTNAME_FILE = TEST_DATA_FOLDER.resolve("emptyEventName.xml");
+    private static final Path WHITESPACE_EVENTNAME_FILE = TEST_DATA_FOLDER.resolve("whitespaceEventName.xml");
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -45,9 +47,18 @@ public class XmlSerializableAwarenessTest {
     }
 
     @Test
-    public void toModelType_emptyventNames_throwsIllegalValueException() throws Exception {
+    public void toModelType_emptyEventName_throwsIllegalValueException() throws Exception {
         XmlSerializableAwareness dataFromFile =
-        XmlSerializableAwareness.loadDataFromSaveFile(EMPTY_EVENTNAME_FILE);
+                XmlSerializableAwareness.loadDataFromSaveFile(EMPTY_EVENTNAME_FILE);
+
+        thrown.expect(IllegalValueException.class);
+        dataFromFile.toModelType();
+    }
+
+    @Test
+    public void toModelType_whitespaceEventName_throwsIllegalValueException() throws Exception {
+        XmlSerializableAwareness dataFromFile =
+                XmlSerializableAwareness.loadDataFromSaveFile(WHITESPACE_EVENTNAME_FILE);
 
         thrown.expect(IllegalValueException.class);
         dataFromFile.toModelType();
