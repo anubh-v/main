b7e0607e683d50be0f952f090881b00d497bd857
anubh-v <e0176401@u.nus.edu>
2018-11-11 20:20:54 +0800

    Reorder import

--- src/test/java/seedu/address/model/awareness/DictionaryTest.java
+++ src/test/java/seedu/address/model/awareness/DictionaryTest.java
@@ -1,7 +1,7 @@
 package seedu.address.model.awareness;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Rule;
